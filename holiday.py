e68f8ff3cf0c3af50ca97c61e91f9fc1acdcd227
Cyrus-Jiang <596935196@qq.com>
2018-09-19 23:44:53 +0800

    节假日判断类

---
+++ holiday.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-  
+
+import json
+import urllib2
+from datetime import datetime
+
+# 调用接口查询类型
+def query(log)
+    date = datetime.now().strftime("%Y-%m-%d")
+    server_url = "http://www.easybots.cn/api/holiday.php?d="
+ 
+    api_url_request = urllib2.Request(server_url+date)
+    api_response = urllib2.urlopen(api_url_request)
+ 
+    api_data= json.loads(api_response.read())
+ 
+    log.info(api_data)
+ 
+    if api_data[date]=='0':
+        log.info('This day is weekday')
+        return 0
+    elif api_data[date]=='1':
+        log.info('This day is weekend')
+        return 1
+    elif api_data[date]=='2':
+        log.info('This day is holiday')
+        return 2
+    else:
+    	log.error('Parsing failure, use datetime')
+        return query_datetime(date)
+
+# 判断今天是否是周一到周五
+def query_datetime(date):
+    today = date.weekday()
+    if today < 4:
+        return 0
+    else      
+        return 1
---
+++ logtest.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+
+from Logger import logger
+log = Logger(logname='/log/wgblog.txt', loglevel="INFO", logger="logtest.py").getlog()
